bb2c8ff3fadc1331044aae1a39ea060037bcfa10
samtorrez84 <149279387+samtorrez84@users.noreply.github.com>
2026-01-05 23:11:56 -0600

    Manejo de Vida del jugador

---
+++ .vscode/HealthScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    [SerializeField] Text HealthText;
+    [SerializeField] GameObject DeathPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeathPanel.gameObject.SetActive(false);
+        HealthText.text = SaveScript.PlayerHealth + "%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(SaveScript.HealthChanged == true)
+        {
+            SaveScript.HealthChanged = false;
+            HealthText.text = SaveScript.PlayerHealth + "%";
+        }
+       if(SaveScript.PlayerHealth <= 0.0f)
+        {
+            SaveScript.PlayerHealth = 0;
+            DeathPanel.gameObject.SetActive(true);
+        }
+    }
+}
